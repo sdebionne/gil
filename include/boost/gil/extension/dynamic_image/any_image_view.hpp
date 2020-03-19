ff8738115ad2bf4c8bcfa997b2400e39ce942083
Samuel Debionne <samuel.debionne@gmail.com>
2020-03-19 16:02:08 +0100

    Use inheriting constructors for any_image and any_image_view

--- include/boost/gil/extension/dynamic_image/any_image_view.hpp
+++ include/boost/gil/extension/dynamic_image/any_image_view.hpp
@@ -82,16 +82,7 @@ public:
     using point_t = point<std::ptrdiff_t>;
     using size_type = std::size_t;
 
-    any_image_view() = default;
-    any_image_view(any_image_view const& view) : parent_t((parent_t const&)view) {}
-
-    template <typename View>
-    explicit any_image_view(View const& view) : parent_t(view) {}
-
-    template <typename ...OtherViews>
-    any_image_view(any_image_view<OtherViews...> const& view)
-        : parent_t((variant2::variant<OtherViews...> const&)view)
-    {}
+    using parent_t::parent_t;
 
     any_image_view& operator=(any_image_view const& view)
     {
